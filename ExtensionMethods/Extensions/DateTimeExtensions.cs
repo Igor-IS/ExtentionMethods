82ed9a86341f03ec7bfbb339d3405fac30f720bc
Igor-IS <igorvulkan@gmail.com>
2020-08-05 13:26:57 -0300

    criando extenções com class static

---
+++ ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Globalization;
+
+
+namespace System //colocando a class extention no mesmo namespace do local de uso, para facilitar o uso!
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj) // necessario que seja criado assim <<
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+            if (duration.TotalHours < 24)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 8, 3);
+            Console.WriteLine(dt.ElapsedTime());
+            Console.WriteLine();
+
+            string text = "Zézinho da tapioca, caiu na rampa";
+            Console.WriteLine(text.Cut(20));
+
+
+        }
+    }
+}
